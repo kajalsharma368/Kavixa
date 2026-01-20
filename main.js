0c2b54e2120215efca70bae4e4fada6370260ec3
kajalsharma368 <kajalsharmamax@gmail.com>
2026-01-20 18:24:08 +0530

    Refactor smooth scroll for sidebar links

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-document.querySelectorAll(".side-nav a").forEach(link => {
-  link.addEventListener("click", e => {
+// Smooth scroll for sidebar links
+document.querySelectorAll('.sidebar nav a').forEach(link => {
+  link.addEventListener('click', e => {
     e.preventDefault();
-    document
-      .querySelector(link.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
+    document.querySelector(link.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
   });
 });
