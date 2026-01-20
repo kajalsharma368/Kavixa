3e694a55ce3160b99d1aae410a41b53fd9d213ce
kajalsharma368 <kajalsharmamax@gmail.com>
2026-01-20 17:25:15 +0530

    Add smooth scroll functionality to navigation
    Implement smooth scrolling for navigation links.

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+// Smooth scroll
+document.querySelectorAll("nav a").forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    document
+      .querySelector(link.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
 
